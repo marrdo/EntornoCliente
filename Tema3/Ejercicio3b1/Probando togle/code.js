d1274a113db66b219a32614924fd97961d369bad
marrdo <manuelmaldonado9330329@gmail.com>
2023-10-10 14:20:16 +0200

    intento de arreglo toggle

--- Tema3/Ejercicio3b1/Probando togle/code.js
+++ Tema3/Ejercicio3b1/Probando togle/code.js
@@ -3,5 +3,6 @@
 let menu = document.querySelector("#datos");
 let btnMostrar= document.querySelector("#btndatos");
 btnMostrar.addEventListener("click",()=>{
-    menu.classList.toggle("divDatos--ocultar")
+    menu.classList.toggle("divDatos--ocultar");
+    menu.classList.toggle("modes_menu--mostrar");
 });
