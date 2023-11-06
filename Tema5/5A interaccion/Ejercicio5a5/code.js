c367854c16a468f20b3e815e8459ad2fbd2c245a
marrdo <manuelmaldonado930329@gmail.com>
2023-11-06 22:06:18 +0100

    ejercicio5a9

--- Tema5/5A interaccion/Ejercicio5a5/code.js
+++ Tema5/5A interaccion/Ejercicio5a5/code.js
@@ -1,35 +1,27 @@
 "use strict";
 
-
-
-
-
-
-
 let arrProvincias = ["Álava","Albacete","Alicante","Almería","Ávila","Badajoz","Baleares","Barcelona","Burgos","Cáceres","Cádiz","Castellón",
 "Ciudad Real","Córdoba","Coruña","Cuenca","Girona","Granada","Guadalajara","Guipuzcoa","Huelva","Huesca","Jaén","León","Lleida",
 "Rioja","Lugo","Madrid","Málaga", "Murcia","Navarra","Orense","Asturias","Palencia","Las Palmas","Pontevedra","Salamanca",
 "Tenerife","Cantabria","Segovia","Sevilla","Soria","Tarragona","Teruel","Toledo","Valencia","Valladolid",
 "Vizcaya","Zamora","Zaragoza","Ceuta","Melilla"];
 
+const mostrarCiudad = document.querySelector(".ciudad");
+let cPostal = document.querySelector("#cp");
 
 
-let btn=document.querySelector("#btn");
+cPostal.addEventListener("keyup", e =>{
+    const codigo = e.target.value;
 
-let nPostal=document.querySelector("#cp").value;
-
-btn.addEventListener("click",()=>{
-    
-    nPostal = Array.from(nPostal);
-    nPostal=Number(nPostal[0]+nPostal[1]);
-        console.log(nPostal);
-        
-    if (nPostal.length == 5) {
-        console.log(typeof(nPostal.length));
+    if(codigo.length != 5){
+        return;
     }
+
+    const provincia = Number(codigo.slice(0,2)-1);
+
+    if(isNaN(provincia)){
+        mostrarCiudad.innerHTML = "Debe introducir solo numeros.";
+    }
+
+    mostrarCiudad.innerHTML = `${arrProvincias[provincia]}`;
 });
-
-
-
-
-
