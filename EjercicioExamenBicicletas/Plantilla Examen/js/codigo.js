4d579853b0527ebcb11248a8f7222e465a50b81d
marrdo <manuelmaldonado930329@gmail.com>
2023-11-17 00:06:08 +0100

    Ejercicio Bicicletas completado

--- EjercicioExamenBicicletas/Plantilla Examen/js/codigo.js
+++ EjercicioExamenBicicletas/Plantilla Examen/js/codigo.js
@@ -9,10 +9,13 @@ function ocultarFormularios(){
 }
 
 
+
 /////////////
 //MAIN
 ////////////
+let tienda = new Tienda();
 
+console.log(tienda);
 
 ocultarFormularios();
 
@@ -21,9 +24,10 @@ ocultarFormularios();
 const btnAltaBici = document.querySelector('#btnMostrarAlta');
 
 btnAltaBici.addEventListener("click", ()=>{
+    ocultarFormularios();
     const formAlta = document.querySelector('form[name = "frmAltaBicicleta"]');
 
-    formAlta.classList.toggle('oculto');
+    formAlta.classList.remove('oculto');
 
 });
 
@@ -35,20 +39,81 @@ btnDarDeAlta.addEventListener("click", ()=>{
     //AÑO
     let anio = document.querySelector('#lstAnio').value;
     //Tipo bici
-    const radiosTipoBici = document.querySelectorAll('input[name="rbtTipoBicicleta"]');
-    let tipoBici ;
-    radiosTipoBici.forEach((tipo)=>{
-        if (tipo.checked) {
-            tipoBici = tipo.value;
+    let tipoBici = document.querySelector('[name="rbtTipoBicicleta"]:checked').value;
+    console.log(tipoBici);
+    if(tipoBici === 'carretera'){//Añadir bicis si son de carretera
+        let numPlatos = document.querySelector('#txtPlatos').value;
+
+        if(localizador == '' || anio == '' || numPlatos == ''){
+            alert('Debe rellenar los campos:Localizador, Año y Numero de platos.');
+            return;
+        }else{
+            let biciCarretera = new Carretera(localizador,anio,false,numPlatos);
+
+            tienda.altaBicicleta(biciCarretera);    
+            console.log(tienda.tBicis);
+            ocultarFormularios();
         }
-    });
+    }else{//Añadir bicis si son de Montaña
+        let numSuspensiones = document.querySelector('#txtSuspensiones').value;
 
-    if (tipo === 'montana') {
-        const numSuspensiones = document.querySelector('#txtSuspensiones');
-    }else{
-        const numPlatos = document.querySelector('#txtPlatos');
+        if(localizador == '' || anio == '' || numSuspensiones == ''){
+            alert('Debe rellenar los campos:Localizador, Año y Numero de suspensiones.');
+            return;
+        }else{
+            let biciMontania = new Montania(localizador,anio,false,numSuspensiones);
+
+            tienda.altaBicicleta(biciMontania);
+            console.log(tienda.tBicis);
+            ocultarFormularios();
+        }
     }
-
 })
 
+/*////////////// VENTA DE BICICLETAS /////////////////////*/
 
+const mostrarVenta = document.querySelector('#btnMostrarVenta');
+
+mostrarVenta.addEventListener('click', () =>{
+    ocultarFormularios();
+    const formVenta =  document.querySelector('[name="frmVentaBicicleta"]');
+    formVenta.classList.remove('oculto');
+});
+
+//Boton de vender bicicleta
+const btnVender = document.querySelector('#btnVentaBicicleta');
+
+btnVender.addEventListener('click', ()=>{
+    let localizadorVenta = document.querySelector('#txtLocalizadorVenta').value;
+
+    tienda.ventaBici(localizadorVenta);
+});
+
+/*  /////////////// MOSTRAR TOTALES ///////////////////// */
+
+//Capturamos el div de totales.
+let divTotales = document.querySelector('#totales');
+//Capturamos el boton de mostrarTotales
+let btnMostrarTotales = document.querySelector('#btnMostrarTotales');
+
+btnMostrarTotales.addEventListener('click', ()=>{
+    divTotales.innerHTML = `<h3>Bicicletas de carretera: ${tienda.numCarretera()}</h3>`;
+    divTotales.innerHTML += `<h3>Bicicletas de montaña: ${tienda.numMontania()}</h3>`;
+    divTotales.innerHTML += `<h3>Total de bicicletas: ${tienda.numTotal()}</h3>`;
+    divTotales.innerHTML += `<h3>Total de ventas: ${tienda.numVenta()}</h3>`;
+});
+
+/*  ///////////////     MOTRAR TABLAS   ////////////    */
+
+
+let btnMostrarListado = document.querySelector('#btnMostrarListado');
+
+btnMostrarListado.addEventListener('click',()=>{
+    let nuevaVentana = window.open();
+
+    nuevaVentana.listadoMontania = tienda.listadoMontania();
+    nuevaVentana.listadoCarretera = tienda.listadoCarretera();
+
+    nuevaVentana.document.write(`<h2>Tabla de Montaña</h2>${nuevaVentana.listadoMontania}`);
+    nuevaVentana.document.write(`<h2>Tabla de Carretera</h2>${nuevaVentana.listadoCarretera}`);
+});
--- EjercicioExamenBicicletas/Plantilla Examen/js/objetos.js
+++ EjercicioExamenBicicletas/Plantilla Examen/js/objetos.js
@@ -1,68 +1,172 @@
 "use strict";
 
-function Bicicleta(localizador, anio, vendida) {
-    this.localizador = localizador;
-    this.anio = anio;
-    this.vendida = vendida;
+function Bicicleta(localizador, anio, vendida = false) {
+  this.localizador = localizador;
+  this.anio = anio;
+  this.vendida = vendida;
 }
 
-Bicicleta.prototype.toHTMLrow = function() {
-    // Implementa la lógica para convertir la bicicleta a una fila HTML
-};
+Bicicleta.prototype.toHTMLrow = function () {
+    return `<tr><td>${this.localizador}</td><td>${this.anio}</td><td>${this.vendida}</td></tr>`;
+  };
 
 function Carretera(localizador, anio, vendida, numPlatos) {
-    Bicicleta.call(this, localizador, anio, vendida);
-    this.numPlatos = numPlatos;
+  Bicicleta.call(this, localizador, anio, vendida);
+  this.numPlatos = numPlatos;
 }
 
 Carretera.prototype = Object.create(Bicicleta.prototype);
 Carretera.prototype.constructor = Carretera;
 
-Carretera.prototype.toHTMLrow = function() {
-    // Implementa la lógica específica para bicicletas de carretera
-};
+Carretera.prototype.toHTMLrow = function () {
+    if (this.vendida == true) {
+        return `<tr><td>${this.localizador}</td><td>${this.anio}</td><td>Vendida</td><td>${this.numPlatos}</td></tr>`;    
+    }else{
+        return `<tr><td>${this.localizador}</td><td>${this.anio}</td><td>En stock</td><td>${this.numPlatos}</td></tr>`;
+    }
+    
+  };
 
 function Montania(localizador, anio, vendida, numSuspensiones) {
-    Bicicleta.call(this, localizador, anio, vendida);
-    this.numSuspensiones = numSuspensiones;
+  Bicicleta.call(this, localizador, anio, vendida);
+  this.numSuspensiones = numSuspensiones;
 }
 
 Montania.prototype = Object.create(Bicicleta.prototype);
 Montania.prototype.constructor = Montania;
 
-Montania.prototype.toHTMLrow = function() {
-    // Implementa la lógica específica para bicicletas de montaña
-};
-
+Montania.prototype.toHTMLrow = function () {
+    if (this.vendida == true) {
+        return `<tr><td>${this.localizador}</td><td>${this.anio}</td><td>Vendida</td><td>${this.numSuspensiones}</td></tr>`;    
+    }else{
+        return `<tr><td>${this.localizador}</td><td>${this.anio}</td><td>En stock</td><td>${this.numSuspensiones}</td></tr>`;
+    }
+    
+  };
 // Instanciar Carretera
 //const carretera = new Carretera('XYZ789', 2021, true, 2);
 
 // Instanciar Montania
 //const montania = new Montania('DEF456', 2023, false, 4);
 
+class Tienda {
+  constructor(tBicis = [], numVentas) {
+    this.tBicis = tBicis;
+    this.numVentas = numVentas;
+  }
 
-class Tienda{
-    constructor(tBicis = [],numVentas){
-        this.tBicis = tBicis;
-        this.numVentas = numVentas;
+  /**
+   * Agrega una nueva bicicleta a la tienda.
+   *
+   * @param {Bicicleta} OBici - Objeto que representa la bicicleta a agregar.
+   * @returns {boolean} - Devuelve true si la bicicleta se agregó con éxito, false si falló.
+   * @memberof Tienda
+   */
+  altaBicicleta(OBici) {
+    //Si el array tiene un tamaño de 0 introduzco la bicicleta
+    if (this.tBicis.length == 0) {
+      this.tBicis.push(OBici);
+      alert("Alta bicicleta OK");
+    } else {
+      //Si hay mas de uno primero compruebo si el localizador de la bici existe en el array.
+      let idBici = OBici.localizador;
+      let biciEnTienda = this.tBicis.find(
+        (objetoQueRecorro) => objetoQueRecorro.localizador == idBici
+      );
+      //Si esta en la tienda mando un alert avisando de que la bici ya esta en la tienda
+      if (biciEnTienda) {
+        alert("Bicicleta registrada previamente");
+      } else {
+        //Si no está en la tienda introduzco la bici en el array y mando el aviso de alerta.
+        this.tBicis.push(OBici);
+        alert("Alta bicicleta OK");
+      }
+    }
+  }
+  /**
+   *
+   * @param {*} sLocalizador
+   * @returns {String}
+   */
+  ventaBici(sLocalizador) {
+    let buscarLocalizador = this.tBicis.find(
+        (bicicleta) => bicicleta.localizador === sLocalizador
+    );
+
+    if (buscarLocalizador) {
+        for (let bicicleta of this.tBicis) {
+            if (bicicleta.localizador === buscarLocalizador.localizador && !bicicleta.vendida) {
+                bicicleta.vendida = true;
+                alert('Bicicleta vendida');
+                return;  // Sale del método después de vender la bicicleta
+            } else if (bicicleta.localizador === buscarLocalizador.localizador && bicicleta.vendida) {
+                alert('Bicicleta ya vendida');
+                return;  // Sale del método si la bicicleta ya está vendida
+            }
+        }
+    } else {
+        alert('La bicicleta no existe.');
+    }
+}
+    numCarretera(){
+        let biciCarretera=0;
+        this.tBicis.forEach((bicicleta)=>{
+            if(bicicleta instanceof Carretera){
+                biciCarretera++;
+            }
+        });
+
+        return biciCarretera;
+    }
+    numMontania(){
+        let biciMontania=0;
+
+        this.tBicis.forEach((bicicleta)=>{
+            if(bicicleta instanceof Montania){
+                biciMontania++;
+            }
+        });
+        return biciMontania;
+
+    }
+    numTotal(){
+        return this.tBicis.length;
+    }
+    numVenta(){
+        let biciVendidas=0;
+
+        this.tBicis.forEach((bicicleta)=>{
+            if(bicicleta.vendida == true){
+                biciVendidas++;
+            }
+        });
+        return biciVendidas;
     }
 
- /**
- * Agrega una nueva bicicleta a la tienda.
- *
- * @param {Bicicleta} OBici - Objeto que representa la bicicleta a agregar.
- * @returns {boolean} - Devuelve true si la bicicleta se agregó con éxito, false si falló.
- * @memberof Tienda
- */
-    altaBicicleta(OBici){
+    listadoMontania(){
+        let s = `<table>`;
+        s += `<thead> <tr><th>Localizador</th><th>Año</th><th>Vendida</th><th>Nº Suspensiones</th></tr></thead>`;
+        s += `<tbody>`;
+        for(let bici of this.tBicis){
+            if (bici instanceof Montania) {
+                s += bici.toHTMLrow();
+            }
+        }
+        s += `</tbody></table>`;
+        return s;
+    }
 
+    listadoCarretera(){
+        let s = `<table>`;
+        s += `<thead> <tr><th>Localizador</th><th>Año</th><th>Vendida</th><th>Nº Platos</th></tr></thead>`;
+        s += `<tbody>`;
+        for(let bici of this.tBicis){
+            if (bici instanceof Carretera) {
+                s += bici.toHTMLrow();
+            }
+        }
+        s += `</tbody></table>`;
+        return s;
     }
-    /**
-     * 
-     * @param {*} sLocalizador 
-     * @returns {String}
-     */
-    ventaBici(sLocalizador){
-        
-    }
+
 }
