27c9335d3dca987d5beebf901153d3d9abb99e3a
marrdo <manuelmaldonado930329@gmail.com>
2023-12-12 20:01:36 +0100

    DOM

---
+++ Tema6/DOM/Ejercicio1/code.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const resul = document.querySelector('#resultado');
+
+resul.firstElementChild.innerHTML = 'Lista de animales';
+
+resul.firstElementChild.nextElementSibling.firstElementChild.innerHTML = 'Lobo'; 
+
+resul.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.innerHTML = 'de agua';
+
