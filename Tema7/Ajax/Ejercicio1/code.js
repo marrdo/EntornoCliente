d4ac6c6a1b8e4cd00ad0efa5ab06ea48ac179134
marrdo <manuelmaldonado930329@gmail.com>
2024-01-08 22:00:13 +0100

    empezando asincornia

---
+++ Tema7/Ajax/Ejercicio1/code.js
@@ -0,0 +1,43 @@
+"use strict";
+
+function loadXMLDOC(filename, callback) {
+  let xhttp;
+  if (window.XMLHttpRequest) {
+    xhttp = new XMLHttpRequest();
+  }
+
+  xhttp.onreadystatechange = function () {
+    if (xhttp.readyState === XMLHttpRequest.DONE) {
+      if (xhttp.status === 200) {
+        callback(xhttp.responseXML);
+        // callback(xhttp.responseText); // si el fichero es de texto
+      } else {
+        console.log("Hubo un error con la petición.");
+      }
+    }
+  };
+  xhttp.open("GET", filename, true); //true = asíncrona, //false = síncrona
+  xhttp.send();
+}
+
+function cargarArchivo(xml) {
+  const resultado = document.querySelector(".resultado");
+  let result = "";
+
+  xml.querySelectorAll("food").forEach((element) => {
+    console.log(element);
+    const name = document.querySelector("name");
+    const price = name.nextElementSibling;
+    const description = price.nextElementSibling;
+    const calories = description.nextElementSibling;
+
+    result += `<li>${name.textContent}</li>
+      <li>${price.textContent}</li>
+      <li>${description.textContent}</li>
+      <li>${calories.textContent}</li>`;
+  });
+
+  resultado.innerHTML += `<ul>${result}</ul>`;
+}
+
+const imprimeXML = loadXMLDOC("w3SchoolFile.xml", cargarArchivo);
